dde9ef2901c22d5d89611b827b27fd8b231a13b2
Primed <primed.5353@gmail.com>
2025-03-28 11:45:47 +0000

    Attempt at pick up object
    Incomplete commit of a pickup object and debugging messages

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,8 +2,10 @@ extends CharacterBody2D
 
 const SPEED = 150      
 const RUN_SPEED = 250  
+var is_near_pickup = false
+var pickup_object = null
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var direction = Vector2.ZERO
 
 	if Input.is_action_pressed("ui_right"):
@@ -22,3 +24,23 @@ func _physics_process(delta):
 
 	velocity = direction.normalized() * current_speed
 	move_and_slide()
+	
+func _on_area_entered(area):
+	print("Player entered area")
+	if area.is_in_group("pickups"):
+		is_near_pickup = true
+		pickup_object = area
+
+func _on_area_exited(area):
+	if area.is_in_group("pickups"):
+		is_near_pickup = false
+		pickup_object = null
+
+func _input(event):
+	if event.is_action_pressed("ui_select") and is_near_pickup:
+		pickup()
+		
+func pickup():
+	if pickup_object:
+		print("Picked up " + pickup_object.name)
+		pickup_object.queue_free()
