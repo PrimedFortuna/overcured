64843506199611e07e48c637e7e2bce6dcee65a5
Primed <primed.5353@gmail.com>
2025-03-28 21:59:17 +0000

    Pickup filter system
    Basically a way to filter types of items tru groups

--- Scripts/PickUpObject.gd
+++ Scripts/PickUpObject.gd
@@ -1,5 +1,11 @@
-extends Area2D
+extends Area2D 
 
 func _ready():
 	print("Pickup script is running!")
-	add_to_group("pickups")
+	add_to_group("pickups")  
+
+	if "Pokeball" in name:
+		add_to_group("pokeballs")
+		
+	if "Berry" in name:
+		add_to_group("berry")
