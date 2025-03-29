a8afc8d25d60b36b6d2c5c30e36d880ccd76fe59
Tomás Salgueiro <116730433+Fang261@users.noreply.github.com>
2025-03-29 22:38:20 +0000

    menu2

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -1,8 +1,5 @@
 extends Control
 
-func _ready():
-	$AnimationPlayer.play("RESET")
-
 func resume():
 	get_tree().paused = false
 	$AnimationPlayer.play_backwards("blur")
@@ -10,7 +7,7 @@ func resume():
 func pause():
 	get_tree().paused = true
 	$AnimationPlayer.play("blur")
-
+	
 func testEsc():
 	if Input.is_action_just_pressed("esc") and !get_tree().paused:
 		pause()
@@ -18,17 +15,17 @@ func testEsc():
 		resume()
 
 
-func _on_resume_pressed():
+func _on_resume_pressed() -> void:
 	resume()
 
 
-func _on_quit_pressed():
-	get_tree().quit()
+func _on_restart_pressed() -> void:
+	get_tree().reload_current_scene()
 
+
+func _on_quit_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+	
+	
 func _process(delta):
 	testEsc()
-
-
-func _on_options_pressed():
-	resume()
-	get_tree().change_scene_to_file("res://options_menu.tscn")
