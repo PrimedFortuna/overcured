954d2a7fcc9dca084854c5d6b6997d93d918d505
Primed <primed.5353@gmail.com>
2025-03-29 09:25:10 +0000

    Various fixes
    Build tested, screen sizes and settings fully implemented

--- Scripts/Settings.gd
+++ Scripts/Settings.gd
@@ -2,6 +2,16 @@ extends Control
 
 func _ready():
 	$BackButton.connect("pressed", _on_back_pressed)
+	$VSyncButton.connect("pressed", _on_vsync_pressed)
+	$FullscreenButton.connect("pressed", _on_fullscreen_pressed)
+#	$SkinButton.connect("pressed",_on_skintoggle_pressed)
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+
+func _on_vsync_pressed():
+	DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED if DisplayServer.window_get_vsync_mode() == DisplayServer.VSYNC_DISABLED else DisplayServer.VSYNC_DISABLED)
+
+func _on_fullscreen_pressed():
+	var is_fullscreen = DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED if is_fullscreen else DisplayServer.WINDOW_MODE_FULLSCREEN)
