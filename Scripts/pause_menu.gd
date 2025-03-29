9d629c9c4061490b2e63dfe083eaa14061d5ef2e
Tomás Salgueiro <116730433+Fang261@users.noreply.github.com>
2025-03-29 22:50:51 +0000

    menu3

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -4,6 +4,12 @@ func resume():
 	get_tree().paused = false
 	$AnimationPlayer.play_backwards("blur")
 
+func restart():
+	get_tree().paused = false
+	$AnimationPlayer.stop()  # Stop the animation immediately
+	$AnimationPlayer.seek(0, true)  # Reset animation to the first frame
+	get_tree().reload_current_scene()
+
 func pause():
 	get_tree().paused = true
 	$AnimationPlayer.play("blur")
@@ -20,11 +26,14 @@ func _on_resume_pressed() -> void:
 
 
 func _on_restart_pressed() -> void:
-	get_tree().reload_current_scene()
+	restart()
 
 
 func _on_quit_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+	get_tree().paused = false
+	var main_menu_scene = load("res://Scenes/MainMenu.tscn")
+	get_tree().change_scene_to_packed(main_menu_scene)
+	
 	
 	
 func _process(delta):
