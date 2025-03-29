53b8dfddca7c5d69cfdbe1ae4039333a6c5b961f
Carlos <94853801+asantos142@users.noreply.github.com>
2025-03-29 14:12:23 +0000

    Adição da troca de skins

---
+++ Assets/Sprites/Sprites.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var charSprites = {
+	0 : preload("res://Assets/Sprites/Joy_Sprites/joy_front_still_1.png"),
+	1 : preload("res://Assets/Sprites/Brendan_Sprites/brendan_down_1.png")
+}
