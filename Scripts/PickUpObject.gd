9b595db6201278a57fc6a179924ff2aab19e63be
Carlos <94853801+asantos142@users.noreply.github.com>
2025-03-29 04:59:41 +0000

    Adição de items

--- Scripts/PickUpObject.gd
+++ Scripts/PickUpObject.gd
@@ -9,3 +9,45 @@ func _ready():
 		
 	if "Berry" in name:
 		add_to_group("berry")
+		
+	if "OranBerry" in name:
+		add_to_group("potion")
+		
+	if "Potion" in name:
+		add_to_group("superPotion")
+		
+	if "EnergyPowder" in name:
+		add_to_group("superPotion")
+		
+	if "Potion" in name:
+		add_to_group("hyperPotion")
+		
+	if "EnergyRoot" in name:
+		add_to_group("hyperPotion")
+		
+	if "HyperPotion" in name:
+		add_to_group("maxPotion")
+		
+	if "OranBerry" in name:
+		add_to_group("antidote")
+	
+	if "PechaBerry" in name:
+		add_to_group("antidote")
+		
+	if "OranBerry" in name:
+		add_to_group("awakening")
+		
+	if "ChestoBerry" in name:
+		add_to_group("awakening")
+		
+	if "OranBerry" in name:
+		add_to_group("burnHeal")
+		
+	if "RawstBerry" in name:
+		add_to_group("burnHeal")
+		
+	if "OranBerry" in name:
+		add_to_group("iceHeal")
+		
+	if "AspearBerry" in name:
+		add_to_group("iceHeal")
