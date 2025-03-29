41d6946cae729df49fe854d0988ca13f807d4f6e
Primed <primed.5353@gmail.com>
2025-03-29 17:30:21 +0000

    Pokemon.gd fundamentals added

--- Scripts/Pokemon.gd
+++ Scripts/Pokemon.gd
@@ -1,1 +1,18 @@
-extends Node
+extends Node2D
+
+var health: int = 100
+var max_health: int = 100
+var status: Array = []
+
+func _ready():
+	print("Pokemon Initialized: ", health, "HP")
+
+func add_status(status_effect: String):
+	status.append(status_effect)
+	print("Status effect added: ", status_effect)
+
+func remove_status(status_effect: String):
+	if status_effect in status:
+		status.erase(status_effect)
+		print("Status effect removed: ", status_effect)
+		
